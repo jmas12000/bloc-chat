e0a74bcc89e0b447313483b02ce2fd2fa5dbfd02
Jeff Moore <jmas1_2000@hotmail.com>
2016-12-17 09:10:30 -0800

    user story 1 list chat rooms: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,36 @@
-(function() {
+var blocChat = angular.module('blocChat', ['firebase', 'ui.router']);
+
+blocChat.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    
     $stateProvider
-    .state('mainView', {
-        url:
-        controller:
-        templateUrl:
-    });
-      
-    angular
-        .module('blocChat', ['ui-router', 'firebase'])
-})();
+        .state('home', {
+            url: '/',
+            controller: 'HomeCtrl as home',
+            templateUrl: '/templates/home.html'
+        });
+    
+}]);
+                
+blocChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
+    var ref = firebase.database().ref().child('rooms');
+    rooms = $firebaseArray(ref);
+    rooms = ["room 1", "room 2", "room 3", "room 4", "room 5", "room 6"];
+    
+   
+    return {
+        all : rooms
+    }
+}]);
+
+blocChat.controller('HomeCtrl', [ '$scope', 'Room', function( $scope, Room) {
+    $scope.rooms = Room.all;
+}]);
+                
+                
+                
+                
